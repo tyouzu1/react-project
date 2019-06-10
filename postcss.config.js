0c4f8b9416bb1aa2b47ab4f1c765691b3676ad66
Raz1o <tyouzu1@163.com>
2019-06-10 15:52:15 +0800

    autofix

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
 /* eslint-disable */
 module.exports = {
   plugins: [
-    require('autoprefixer')({
-      browsers: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 8', 'iOS >= 8', 'Android >= 4'] 
-    }),
+    require('autoprefixer')(),
     require('cssnano')()
   ],
 };
